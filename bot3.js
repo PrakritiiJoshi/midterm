8974125ebbaf25b2eec266b1017ce8af16fb22b8
PrakritiiJoshi <31702271+PrakritiiJoshi@users.noreply.github.com>
2018-10-20 10:37:36 -0400

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -10,6 +10,8 @@ var T = new Twit(config);
 var username;
 var location;
 
+
+// connecting weather api
 const request = require('request');
 
 let apiKey = '...'; 
@@ -32,24 +34,26 @@ request(url, function (err, response, body) {
 })
 
 
-// Setting up a user stream
+// Setting up a user stream and tracking when someone tweets at the bot
 var stream = T.stream('statuses/filter', 
 	{ track: ['locationbott'] }
 	)
 
+//getting the data
 stream.on('message', function (data) {
  //console.log(data);
  username = data.user.screen_name;
  location = data.place.name;
  console.log(location);
  
-
+//responding if specific username tweets the bot
 if (username = 'pritijo49720636') {
 	
 		  var tweet = {
 		  	status: '@' + data.user.screen_name + ' thank you for tweeting me!:) ' +  'It is ' + weather.main.temp +  ' degrees in ' + weather.name + ' at ' + Date()
 		}
 
+//posting to twitter tweet
 T.post('statuses/update',  tweet, tweeted);
 
 
